fa8c98a6761140383b8bb66fa6481e90f6ce9eeb
rawanfatima <rawanfatima@outlook.com>
2024-08-27 15:17:11 -0700

    updated the js

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ switchElement.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     
 })
+document.querySelectorAll('.project-card').forEach(card => {
+    card.addEventListener('click', () => {
+        // Remove 'active' class from other cards
+        document.querySelectorAll('.project-card').forEach(otherCard => {
+            if (otherCard !== card) {
+                otherCard.classList.remove('active');
+            }
+        });
+        
+        // Toggle 'active' class on the clicked card
+        card.classList.toggle('active');
+    });
+});
